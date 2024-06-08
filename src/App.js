141a110ed00bf19550635ec7e36bb2135ba1eaf6
Kat Grace <katherine.grace.reed@gmail.com>
2024-06-08 09:53:20 +0100

    removed React logo
    Signed-off-by: Kat Grace <katherine.grace.reed@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import Header from './components/Header';
 import Profile from './pages/Profile';
 import Projects from './pages/Projects';
 import ProjectDetail from './pages/ProjectDetail';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header logo={logo} />
+        <Header />
         <Routes>
           <Route path="/" element={<Profile userName="Sgeulaiche" />} />
           <Route path="/projects" element={<Projects userName="Sgeulaiche" />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import { Link as RouterLink } from 'react-router-dom';
 export default function Header({ logo }) {
 	return (
 		<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
 			<h1>My Portfolio</h1>
 			<nav>
 				<RouterLink to="/" className="App-link">
